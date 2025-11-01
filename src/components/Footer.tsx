1db5035c4f3a0f6f0d74d81ad95b9458fad14081
Malaika Jahangir <159516809+MalaikaJahangir@users.noreply.github.com>
2025-11-01 12:36:37 +0500

    Fix Instagram link in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -23,7 +23,7 @@ const Footer = () => {
                 <Linkedin className="w-5 h-5" />
               </a>
               <a
-                href="https://www.instagram.com/malaika_jahangir_/"
+                href="https://www.instagram.com/malaikajahangir_/"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="w-10 h-10 bg-gray-800 rounded-full flex items-center justify-center hover:bg-purple-600 transition-colors duration-300"
